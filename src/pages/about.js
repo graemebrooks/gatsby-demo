709e45660f42d80b25d617f870dc9f8591fe5029
Kelso <graemebrooks@gmail.com>
2020-03-30 16:52:55 -0500

    adds header and footer components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const AboutPAge = () => {
+import Footer from '../components/footer';
+import Header from '../components/header';
+
+const AboutPage = () => {
 	return (
 		<div>
+			<Header />
 			<h1>Gatsby Bootcamp About</h1>
 			<p>This is a demo application for the purpose of learning GatsbyJS</p>
 			<p>
 				For any questions, feel free to <Link to="/contact">contact me</Link>
 			</p>
+			<Footer />
 		</div>
 	);
 };
 
-export default AboutPAge;
+export default AboutPage;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import Footer from '../components/footer';
+import Header from '../components/header';
+
 const ContactPage = () => {
 	return (
 		<div>
+			<Header />
 			<h1>Contact Details</h1>
 			<p>
 				The best way to contact me is via my{' '}
-				<a href="https://www.linkedin.com/in/graeme-kelso-brooks/">Linkedin</a>
+				<a href="https://www.linkedin.com/in/graeme-kelso-brooks/" target="_blank">
+					Linkedin
+				</a>
 			</p>
+			<Footer />
 		</div>
 	);
 };
